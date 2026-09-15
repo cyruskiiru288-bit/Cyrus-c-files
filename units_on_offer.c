940e101279f349493c36e9adc4509a1b8829bacf
Cyrus Kiiru <cyruskiiru288@gmail.com>
2026-09-15 20:05:06 +0300

    Add files via upload

---
+++ units_on_offer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//single line comment 
+/*this 
+is a 
+multiline 
+comment*/
+int main()
+{
+    printf("Welcome to Zetech university where you invent your future.\n");
+    printf("The units offered in Bcs firsst year first semester are as follows:\n");
+    printf("1.Structured programming and algorithms.\n");
+    printf("2.Communication skills.\n");
+    printf("3.Basic mathematics.\n");
+    printf("4.Health and wellness management.\n");
+    printf("5.introduction to physics.\n");
+    printf("6.Computational logic.\n");
+    printf("7.Introduction to computer systems and internet.");
+    return 0;
+}
